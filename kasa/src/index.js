9e6a7e57707f9e81f8bc466dccb5e91c66432649
VangitParis <102918421+VangitParis@users.noreply.github.com>
2022-11-02 08:57:56 +0100

    CLEAN

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,29 +1,18 @@
-import * as React from "react";
-import {createRoot} from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './components/Router/App'
-import { createGlobalStyle } from 'styled-components'
 import Header from '../src/components/Layouts/Header/nav'
 import './styles/Saas/utils/_mixins.scss'
 import Footer from '../src/components/Layouts/Footer'
 
-const GlobalStyle = createGlobalStyle`
-*{
-  font-family: 'Montserrat', sans-serif;
-  font-weight:500;
-  font-style: normal;
-  width:100%;
-}
-
-`
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render( // RENDER EST UNE METHODE A LAQUELLE ON PASSE NOS ARGUMENTS
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+root.render(
+  // RENDER EST UNE METHODE A LAQUELLE ON PASSE NOS ARGUMENTS
   <BrowserRouter>
-    <GlobalStyle/>
-    <Header/>
+    <Header />
     <App />
-    <Footer/>
+    <Footer />
   </BrowserRouter>
-  
-);
+)
